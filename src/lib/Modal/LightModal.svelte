e5e5aadd5003b8507b9f5000e4f076cc576bf554
Mattias Persson <mattias.persson.87@gmail.com>
2023-12-31 14:45:23 +0100

    fix selTab lightmodal

--- src/lib/Modal/LightModal.svelte
+++ src/lib/Modal/LightModal.svelte
@@ -13,35 +13,32 @@
 
 	let debounce = false;
 	let timeout: ReturnType<typeof setTimeout>;
-	let selTab: string;
 	let rangeValue = 0;
-	let supportMappings: Record<string, string>;
-	let displayMappings: Record<string, string>;
 
 	let entity: HassEntity;
 	$: if ($states?.[sel?.entity_id]?.last_updated !== entity?.last_updated) {
 		entity = $states?.[sel?.entity_id];
 	}
 
-	// color
-	$: supportedColorModes = entity?.attributes?.supported_color_modes;
-	$: colorMode = entity?.attributes?.color_mode;
-	$: tempSelected = selTab === 'temp' || (colorMode === 'color_temp' && selTab !== 'color');
-	$: colorSelected = selTab === 'color' || (colorMode === 'xy' && selTab !== 'temp');
-
-	$: current = Math.round(rangeValue / 2.55);
-	$: brightness = entity?.attributes?.brightness;
-
-	$: supportMappings = {
+	const supportMappings: Record<string, string> = {
 		brightness: 'brightness',
-		color_temp: 'temp',
+		color_temp: 'color_temp',
 		xy: 'color'
 	};
-	$: displayMappings = {
+
+	const displayMappings: Record<string, string> = {
 		brightness: $lang('brightness'),
 		color_temp: $lang('color_temp'),
 		xy: $lang('color')
 	};
+
+	// color
+	$: supportedColorModes = entity?.attributes?.supported_color_modes;
+	$: colorMode = entity?.attributes?.color_mode;
+	$: selTab = supportMappings?.[colorMode];
+
+	$: current = Math.round(rangeValue / 2.55);
+	$: brightness = entity?.attributes?.brightness;
 
 	const masterEntity: HassEntity = $states?.[sel?.entity_id];
 	let groupSelected: string | undefined;
@@ -53,8 +50,10 @@
 {#if isOpen}
 	<Modal>
 		<h1 slot="title">{getName(sel, entity)}</h1>
+
 		{#if masterEntity?.attributes?.entity_id}
 			<h2>{$lang('group')}</h2>
+
 			<div class="button-container">
 				<button
 					class:selected={$states?.[masterEntity?.entity_id]?.entity_id === groupSelected}
@@ -88,7 +87,7 @@
 		{/if}
 
 		<!-- BRIGHTNESS -->
-		{#if brightness !== undefined}
+		{#if brightness}
 			<h2>
 				{$lang('brightness')}
 				<span class="align-right">
@@ -103,23 +102,29 @@
 			<h2>{$lang('change_color')}</h2>
 		{/if}
 
-		{#if supportedColorModes?.length > 1}
+		{#if supportedColorModes?.length > 0}
 			<div class="button-container">
-				{#each supportedColorModes as mode, index}
+				{#each supportedColorModes as mode}
 					<button
-						class:selected={mode}
+						class:selected={selTab === supportMappings?.[mode]}
 						on:click={() => {
-							selTab = supportMappings[mode];
+							selTab = supportMappings?.[mode];
 						}}
 						use:Ripple={$ripple}
 					>
-						{displayMappings[mode]}
+						{displayMappings?.[mode]}
 					</button>
 				{/each}
 			</div>
 		{/if}
 		{#if supportedColorModes?.includes('color_temp') || supportedColorModes?.includes('xy')}
-			<ColorPicker {entity} {colorMode} {supportedColorModes} {tempSelected} {colorSelected} />
+			<ColorPicker
+				{entity}
+				{colorMode}
+				{supportedColorModes}
+				tempSelected={selTab === 'color_temp'}
+				colorSelected={selTab === 'color'}
+			/>
 		{/if}
 
 		<ConfigButtons />
