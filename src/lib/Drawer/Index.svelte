e184599d1bb507f67f35d6d85f7b3c588cdf7f56
Janis Kelemen <janis@helpspace.com>
2024-01-04 17:00:05 +0100

    Update Index.svelte

--- src/lib/Drawer/Index.svelte
+++ src/lib/Drawer/Index.svelte
@@ -125,12 +125,4 @@
 		gap: 0.5rem;
 		overflow: hidden;
 	}
-
-	hr {
-		border-top: none;
-		border-right: var(--theme-colors-sidebar-border);
-		border-bottom: none;
-		border-left: 1px solid rgba(0, 0, 0, 0.1);
-		margin: 0 0.4rem;
-	}
 </style>
