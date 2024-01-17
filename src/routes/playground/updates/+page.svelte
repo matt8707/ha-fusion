7e3248309a51bd306bb98cd157d97f085dc4e060
Mattias Persson <mattias.persson.87@gmail.com>
2024-01-17 17:04:23 +0100

    rm playground

--- src/routes/playground/updates/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Update from '$lib/Playground/Update.svelte';
-</script>
-
-<h1>Update</h1>
-<Update latest="sensor.hass_release_notes" latest_beta="sensor.hass_release_notes_beta" />
