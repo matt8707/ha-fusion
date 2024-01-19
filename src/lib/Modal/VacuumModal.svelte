8648038fdfd31948591366e0bac9c73ee6191e4e
Mattias Persson <mattias.persson.87@gmail.com>
2024-01-19 13:36:16 +0100

    Add vacuum modal

--- src/lib/Modal/VacuumModal.svelte
+++ src/lib/Modal/VacuumModal.svelte
@@ -6,6 +6,7 @@
 	import Select from '$lib/Components/Select.svelte';
 	import { callService } from 'home-assistant-js-websocket';
 	import Ripple from 'svelte-ripple';
+	import Icon from '@iconify/svelte';
 
 	export let isOpen: boolean;
 	export let sel: any;
@@ -86,7 +87,8 @@
 			<br />
 
 			{#if supports?.BATTERY}
-				{$lang('battery')}: {attributes?.battery_level} %
+				<h2>{$lang('battery')}</h2>
+				{attributes?.battery_level} %
 			{/if}
 		{/if}
 
@@ -101,33 +103,94 @@
 			/>
 		{/if}
 
-		<h2>{$lang('buttons')}</h2>
+		<h2>{$lang('vacuum_commands')?.replace(':', '')}</h2>
 
-		<div>
-			<button class="action" on:click={() => handleClick('start')} use:Ripple={$ripple}>
-				{$lang('start')}
-			</button>
+		<div class="button-container">
+			{#if supports?.TURN_ON}
+				<button
+					title={$lang('on')}
+					class:selected={entity?.state === 'on'}
+					on:click={() => handleClick('turn_on')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon" style="transform: scale(0.7);">
+						<Icon icon="mdi:power-on" height="none" />
+					</div>
+				</button>
+			{/if}
 
-			<button class="action" on:click={() => handleClick('pause')} use:Ripple={$ripple}>
-				{$lang('pause')}
-			</button>
+			{#if supports?.TURN_OFF}
+				<button
+					title={$lang('off')}
+					class:selected={entity?.state === 'off'}
+					on:click={() => handleClick('turn_off')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon" style="transform: scale(0.7);">
+						<Icon icon="mdi:power-off" height="none" />
+					</div>
+				</button>
+			{/if}
 
-			<button class="action" on:click={() => handleClick('stop')} use:Ripple={$ripple}>
-				{$lang('stop')}
-			</button>
+			{#if supports?.START}
+				<button
+					title={$lang('start')}
+					class:selected={entity?.state === 'cleaning'}
+					on:click={() => handleClick('start')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon">
+						<Icon icon="ic:round-play-arrow" height="none" />
+					</div>
+				</button>
+			{/if}
 
-			<button class="action" on:click={() => handleClick('locate')} use:Ripple={$ripple}>
-				{$lang('locate')}
-			</button>
+			{#if supports?.PAUSE}
+				<button
+					title={$lang('pause')}
+					class:selected={entity?.state === 'paused'}
+					on:click={() => handleClick('pause')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon">
+						<Icon icon="ic:round-pause" height="none" />
+					</div>
+				</button>
+			{/if}
 
-			<button
-				class="action"
-				on:click={() => handleClick('return_to_base')}
-				use:Ripple={$ripple}
-				style:opacity={state === 'docked' ? '0.2' : '1'}
-			>
-				{$lang('return_home')}
-			</button>
+			{#if supports?.STOP}
+				<button
+					title={$lang('stop')}
+					class:selected={entity?.state === 'idle'}
+					on:click={() => handleClick('stop')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon">
+						<Icon icon="ic:round-stop" height="none" />
+					</div>
+				</button>
+			{/if}
+
+			{#if supports?.LOCATE}
+				<button title={$lang('locate')} on:click={() => handleClick('locate')} use:Ripple={$ripple}>
+					<div class="icon" style="transform: scale(0.65);">
+						<Icon icon="fa:search" height="none" />
+					</div>
+				</button>
+			{/if}
+
+			{#if supports?.RETURN_HOME}
+				<button
+					title={$lang('return_home')}
+					class:selected={entity?.state === 'returning'}
+					on:click={() => handleClick('return_to_base')}
+					use:Ripple={$ripple}
+				>
+					<div class="icon" style="transform: scale(0.85);">
+						<Icon icon="ic:round-home" height="none" />
+					</div>
+				</button>
+			{/if}
 		</div>
 
 		<!-- <h2>Supports</h2>
@@ -141,8 +204,14 @@
 {/if}
 
 <style>
-	.action {
-		color: black !important;
-		margin: 0.2rem;
+	.button-container > button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.icon {
+		width: 1.6rem;
+		height: 1.6rem;
 	}
 </style>
